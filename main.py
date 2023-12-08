3c762fa9c05be5dd7af3934adb62c866340d3bc0
KwintenDc <r0928467@student.vives.be>
2023-12-08 17:22:01 +0100

    Add Scenary objects

--- main.py
+++ main.py
@@ -76,8 +76,32 @@ class Platform(GameObject):
         super().__init__(x, y, width, height, color)
 
 class Obstacles(GameObject):
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, obstacleType):
         super().__init__(x, y, width, height, color)
+        self.obstacleType = obstacleType
+        self.sprite_sheet = pygame.image.load("smb_scenery_sheet.png")
+        self.pipe_frames = []
+        self.castle_frames = []
+        self.flag_frames = []
+
+        self.pipe_frames.append(pygame.transform.scale(self.sprite_sheet.subsurface(230, 390, 32, 50), (self.width, self.height)))
+
+        self.castle_frames.append(pygame.transform.scale(self.sprite_sheet.subsurface(247, 865, 80, 70), (self.width, self.height)))
+
+        self.flag_frames.append(pygame.transform.scale(self.sprite_sheet.subsurface(250, 593, 22, 170), (self.width, self.height)))
+    
+    def draw(self, screen):
+        if(self.obstacleType == "PIPE"):
+            frame = self.pipe_frames[0]
+            screen.blit(frame, (self.x, self.y))
+        elif(self.obstacleType == "CASTLE"):
+            frame = self.castle_frames[0]
+            screen.blit(frame, (self.x, self.y))
+        elif(self.obstacleType == "FLAG"):
+            frame = self.flag_frames[0]
+            screen.blit(frame, (self.x, self.y))
+        elif(self.obstacleType == "OTHER"):
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
 class Enemies(GameObject):
     def __init__(self, x, y, width, height, color, speed, walking_left, sprite_sheet_path):
@@ -162,21 +186,15 @@ def MoveEnemy(enemies, player, objectsMoving):
     for enemy in enemies:
         if player.MovingDirection == "RIGHT":
             if enemy.walking_left == True:
-                if player.x < SCREEN_WIDTH / 2:
-                    enemy.x -= enemy.speed
-                else: 
-                    if objectsMoving:
-                        enemy.x -= (enemy.speed + player.speed)
-                    else:
-                        enemy.x -= enemy.speed
-            elif enemy.walking_left == False:
-                if player.x < SCREEN_WIDTH / 2:
-                    enemy.x += enemy.speed
+                if objectsMoving:
+                    enemy.x -= (enemy.speed + player.speed)
                 else:
-                    if objectsMoving:
-                        enemy.x += enemy.speed - player.speed
-                    else:
-                        enemy.x += enemy.speed
+                    enemy.x -= enemy.speed 
+            elif enemy.walking_left == False:
+                if objectsMoving:
+                    enemy.x += enemy.speed - player.speed
+                else:
+                    enemy.x += enemy.speed             
         else: 
             if enemy.walking_left == True:
                 enemy.x -= enemy.speed
@@ -259,25 +277,30 @@ def reset_game():
     obstacles_data = [
         # (x, y, width, height, color)
         # [x , SCREEN_HEIGHT-GROUND_THICKNESS-height, width, height, color]
-        [1000, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY],
-        [1500, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY],
-        [1750, SCREEN_HEIGHT-GROUND_THICKNESS-250, 75, 250, COLOR_GRAY],
+        [1000, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY, "PIPE"],
+        [1500, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY, "PIPE"],
+        [1750, SCREEN_HEIGHT-GROUND_THICKNESS-250, 75, 250, COLOR_GRAY, "PIPE"],
 
-        [3650, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN],
-        [3650 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN],
-        [3650 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN],
-        [3650 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN],
+        [3650, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN, "OTHER"],
+        [3650 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN, "OTHER"],
+        [3650 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN, "OTHER"],
+        [3650 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN, "OTHER"],
 
-        [4100, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN],
-        [4100 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN],
-        [4100 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN],
-        [4100 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN],
+        [4100, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN, "OTHER"],
+        [4100 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN, "OTHER"],
+        [4100 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN, "OTHER"],
+        [4100 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN, "OTHER"],
+
+        [5600, SCREEN_HEIGHT-GROUND_THICKNESS-330, 50, 330, COLOR_GRAY, "FLAG"],
+
+        [5700, SCREEN_HEIGHT-GROUND_THICKNESS-300, 280, 300, COLOR_GRAY, "CASTLE"],
     ]
 
-    obstacles = [Obstacles(x, y, width, height, color) for x, y, width, height, color in obstacles_data]
+    obstacles = [Obstacles(x, y, width, height, color, obstacleType) for x, y, width, height, color, obstacleType in obstacles_data]
 
     enemies_data = [
-        [1500 - ENEMY_WIDTH, SCREEN_HEIGHT-GROUND_THICKNESS - ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_COLOR, ENEMY_SPEED, True, ENEMIES_SPRITE_SHEET_PATH]
+        [1500 - ENEMY_WIDTH, SCREEN_HEIGHT-GROUND_THICKNESS - ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_COLOR, ENEMY_SPEED, True, ENEMIES_SPRITE_SHEET_PATH],
+        [1300 - ENEMY_WIDTH, SCREEN_HEIGHT-GROUND_THICKNESS - ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_COLOR, ENEMY_SPEED, True, ENEMIES_SPRITE_SHEET_PATH],
     ] 
 
     enemies = [Enemies(x, y, width, height, color, speed, walking_left, ENEMIES_SPRITE_SHEET_PATH) for x, y, width, height, color, speed, walking_left, ENEMIES_SPRITE_SHEET_PATH in enemies_data]
@@ -300,65 +323,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Platform Game")
 
-    # Initialize player position, velocity, and jump state
-    player_x, player_y = 400 - PLAYER_WIDTH / 2, SCREEN_HEIGHT - GROUND_THICKNESS - PLAYER_HEIGHT
-    player_vel_y = 0
-    onGround = False
-    isJumping = False
-    movingDirection = "STANDING"
-    objectsMoving = False
-
-    player = Player(player_x, player_y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_SPRITE_SHEET_PATH, player_vel_y, PLAYER_SPEED, onGround, isJumping, movingDirection)
-
-    # Initialize enenmy states
-    walking_left = True
-
-    # Create platforms
-    platforms_data = [
-        # (x, y, width, height, color)
-        [450, 400, 200, PLATFORM_THICKNESS, COLOR_BROWN],
-        [2500, 400, 200, PLATFORM_THICKNESS, COLOR_BROWN],
-        [2800, 300, 200, PLATFORM_THICKNESS, COLOR_BROWN],
-    ]
-
-    platforms = [Platform(x, y, width, height, color) for x, y, width, height, color in platforms_data]
-
-    ground_platforms_data = [
-        # (x, y, width, height, color)
-        [0, SCREEN_HEIGHT - GROUND_THICKNESS, 2000, GROUND_THICKNESS, GROUND_COLOR],
-        [2000 + 200, SCREEN_HEIGHT - GROUND_THICKNESS, 2200, GROUND_THICKNESS, GROUND_COLOR],
-        [4400 + 200, SCREEN_HEIGHT - GROUND_THICKNESS, 2000, GROUND_THICKNESS, GROUND_COLOR],
-    ]
-
-    ground_platforms = [Platform(x, y, width, height, color) for x, y, width, height, color in ground_platforms_data]
-
-    #Create obstacles
-    obstacles_data = [
-        # (x, y, width, height, color)
-        # [x , SCREEN_HEIGHT-GROUND_THICKNESS-height, width, height, color]
-        [1000, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY],
-        [1500, SCREEN_HEIGHT-GROUND_THICKNESS-150, 75, 150, COLOR_GRAY],
-        [1750, SCREEN_HEIGHT-GROUND_THICKNESS-250, 75, 250, COLOR_GRAY],
-
-        [3650, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN],
-        [3650 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN],
-        [3650 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN],
-        [3650 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN],
-
-        [4100, SCREEN_HEIGHT-GROUND_THICKNESS-140, 75, 140, COLOR_DARK_BROWN],
-        [4100 + 75, SCREEN_HEIGHT-GROUND_THICKNESS-105, 75, 105, COLOR_DARK_BROWN],
-        [4100 + 75 * 2, SCREEN_HEIGHT-GROUND_THICKNESS-70, 75, 70, COLOR_DARK_BROWN],
-        [4100 + 75 * 3, SCREEN_HEIGHT-GROUND_THICKNESS-35, 75, 35, COLOR_DARK_BROWN],
-    ]
-
-    obstacles = [Obstacles(x, y, width, height, color) for x, y, width, height, color in obstacles_data]
-
-    enemies_data = [
-        [1500 - ENEMY_WIDTH, SCREEN_HEIGHT-GROUND_THICKNESS - ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_COLOR, ENEMY_SPEED, True, ENEMIES_SPRITE_SHEET_PATH]
-    ] 
-
-    enemies = [Enemies(x, y, width, height, color, speed, walking_left, ENEMIES_SPRITE_SHEET_PATH) for x, y, width, height, color, speed, walking_left, ENEMIES_SPRITE_SHEET_PATH in enemies_data]
-
+    player, enemies, platforms, ground_platforms, obstacles, lose, objectsMoving = reset_game()
     # Game loop
     running = True
     lose = False
@@ -483,19 +448,19 @@ def main():
                     platform.draw(screen)
 
             for ground in ground_platforms:
-                if ground.x + ground.width < -300:
+                if ground.x + ground.width < - SCREEN_WIDTH / 2:
                     ground_platforms.remove(ground)
                 else:
                     ground.draw(screen)
             
             for obstacle in obstacles:
-                if obstacle.x + obstacle.width < -300:
+                if obstacle.x + obstacle.width < - SCREEN_WIDTH / 2:
                     obstacles.remove(obstacle)
                 else:
                     obstacle.draw(screen)
             
             for enemy in enemies:
-                if enemy.x + enemy.width < -300:
+                if enemy.x + enemy.width < - SCREEN_WIDTH / 2:
                     enemies.remove(enemy)
                 else:
                     enemy.draw(screen)
